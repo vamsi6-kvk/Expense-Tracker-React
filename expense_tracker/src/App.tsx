2862b50afd01a87289e1375c2af2f29190b2f113
vamsi6-kvk <vamsi6.kvk@gmail.com>
2024-05-17 19:05:28 +0200

    Added validation and submit logic

--- expense_tracker/src/App.tsx
+++ expense_tracker/src/App.tsx
@@ -4,8 +4,7 @@ import "./App.css";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCtaegroy] = useState("");
@@ -23,7 +22,11 @@ function App() {
   return (
     <div>
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
       <div className="mb-3">
         <ExpenseFilter
