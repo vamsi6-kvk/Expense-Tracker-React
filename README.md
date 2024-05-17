6cc42b5ccbca9d351fa6b7af5fcbc63be2cfce20
vamsi6-kvk <vamsi6.kvk@gmail.com>
2024-05-17 16:45:29 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Begineer Projects
+
+## Introduction
+
+A simple expense tracker which allows to add expenses with description, amount under categories Groceries,
+Entertainment, Utilities.
+
+## Tech Stack
+
+- Bootstrap
+- React
+- React Hooks
+- Zod
---
+++ expense_tracker/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
+
+function App() {
+  const [selectedCategory, setSelectedCtaegroy] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCtaegroy(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
+}
+
+export default App;
